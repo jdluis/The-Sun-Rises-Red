b5fcda20d96016b06c8032feb4ba0beb807ea4dd
jdluis <jdluisdev@gmail.com>
2023-01-27 12:21:54 +0000

    html basic, css minimum, view logic add

--- app/main.js
+++ app/main.js
@@ -1,1 +1,26 @@
-console.log("Hello Player")
+/* | | | |  -->> ªªª Global Variables ªªª <<-- | | | | */
+
+//Canvas Init
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+//Views
+const startGameView = document.getElementById('startGameView');
+const gameView = document.querySelector('.canvas-container');
+const winView = document.getElementById('startGameView');
+const gameOverView = document.getElementById('startGameView');
+
+//Buttons
+const startGameBtn = document.getElementById('startGame');
+const restartGame = document.getElementById('restartGame');
+const playAgain = document.getElementById('playAgain');
+/* | | | |  -->> ªªª Main Functions ªªª <<-- | | | | */
+
+
+
+/* | | | |  -->> ªªª Event Listeners ªªª <<-- | | | | */
+
+startGameBtn.addEventListener('click', () => {
+    startGameView.style.display = 'none';
+    gameView.style.display = 'flex';
+});
